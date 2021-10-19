b6d3a67e96db71b107b83395b15f864c16f3774c
Alexander Jocobi <vazquez_alex@outlook.com>
2021-10-18 17:37:51 -0700

    v1 api

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database:{
-        host: 'localhost',
-        user: '',
-        password: '',
-        database: ''
+        host: 'bwie5f40xsrwq7co8zlx-mysql.services.clever-cloud.com',
+        user: 'uduquhxw1dm4vkxv',
+        password: 'EpxS1AEY2to3S0u6pvWt',
+        database: 'bwie5f40xsrwq7co8zlx'
     }
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../database');
-router.get('/Cuadernos', (req, res) =>{
-    pool.query('SELECT * FROM cuadernos', (err, rows, fields) =>{
-        if(!err){
+router.get('/cuadernos', (req, res) =>{
+    pool.query('SELECT * FROM cuadernos', (err, rows) => {
+        if (!err) {
             res.json(rows);
         }else {
             console.log(err);
         }
     })
-})
-router.get('/Cuadernos/:id', (req, res) => {
-    const { id } =req.params;
-    pool.query('SELECT * FROM cuadernos WHERE id = ?', [id], (err, rows, fields) =>{
-        if(!err){
-            res.json(rows);
-        }else {
-            console.log(err);
-        }
-    })
-})
+});
 
+router.get('/cuadernos/:id', (req, res) => {
+    const { id } =req.params;
+    pool.query('SELECT * FROM cuadernos WHERE id = ?', [id], (err, rows) => {
+        if (!err) {
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    })
+});
 
 module.exports = router;
