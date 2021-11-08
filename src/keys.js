8656e567e53a8f3cd2e6f2172f541df47d7c9bde
Uziel <Uziel.estrada@g-global.com>
2021-11-07 19:28:25 -0800

    add new changes

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database:{
-        host: 'bwie5f40xsrwq7co8zlx-mysql.services.clever-cloud.com',
-        user: 'uduquhxw1dm4vkxv',
-        password: 'EpxS1AEY2to3S0u6pvWt',
-        database: 'bwie5f40xsrwq7co8zlx'
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'cotizar'
     }
 };
