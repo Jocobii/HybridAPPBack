e09c6d7b0907592ae8f54a914dbe3a91b0dd5c58
Alexander Jocobi <vazquez_alex@outlook.com>
2021-11-07 22:30:51 -0800

    obtener ultima lista  insertada

--- src/routes/lista.js
+++ src/routes/lista.js
@@ -4,10 +4,29 @@ const pool = require('../database');
 
 router.post('/lista/create', (req, res) => {
     const { namePerson, usuarioId } = req.body;
+    if(!namePerson || !usuarioId) {
+        res.status(400).send('Parametros incorrectos');
+
+        return;
+    }
     pool.query('INSERT INTO lista(name_person, usuario_id) VALUES (?,?)', [namePerson, usuarioId], (err, rows) => {
         if (!err) {
-            res.json(req.body);
+            console.log(rows);
+            res.status(200).send({status: 200, data: rows.insertId});
         } else {
+            res.status(500).send({status: 500, message: 'Ha surgido un error en el servidor'});
+            console.log(err);
+        }
+    })
+})
+
+router.get('/milista', (req, res) => {
+    pool.query('SELECT * FROM lista WHERE id = (SELECT id FROM lista ORDER BY id DESC LIMIT 1)', (err, rows) => {
+        if (!err) {
+            console.log(rows);
+            res.status(200).send({rows});
+        } else {
+            res.status(500).send({status: 500, message: 'Ha surgido un error en el servidor'});
             console.log(err);
         }
     })
